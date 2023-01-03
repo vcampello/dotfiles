b8cc6c7c1e68fa7d4a3f1879078bbc2b297c440f
Victor Campello <victor@ticker.co.uk>
2023-01-03 12:32:01 +0000

    Changed theme to modified afterglow

--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/home/vcampello/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/home/vcampello/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/home/vcampello/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/home/vcampello/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/home/vcampello/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
+local package_path_str = "/Users/victorcampello/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/Users/victorcampello/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/Users/victorcampello/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/Users/victorcampello/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/Users/victorcampello/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -76,132 +76,132 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   LuaSnip = {
     loaded = true,
-    path = "/home/vcampello/.local/share/nvim/site/pack/packer/start/LuaSnip",
+    path = "/Users/victorcampello/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
   ["cmp-buffer"] = {
     loaded = true,
-    path = "/home/vcampello/.local/share/nvim/site/pack/packer/start/cmp-buffer",
+    path = "/Users/victorcampello/.local/share/nvim/site/pack/packer/start/cmp-buffer",
     url = "https://github.com/hrsh7th/cmp-buffer"
   },
   ["cmp-nvim-lsp"] = {
     loaded = true,
-    path = "/home/vcampello/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
+    path = "/Users/victorcampello/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
   ["cmp-nvim-lua"] = {
     loaded = true,
-    path = "/home/vcampello/.local/share/nvim/site/pack/packer/start/cmp-nvim-lua",
+    path = "/Users/victorcampello/.local/share/nvim/site/pack/packer/start/cmp-nvim-lua",
     url = "https://github.com/hrsh7th/cmp-nvim-lua"
   },
   ["cmp-path"] = {
     loaded = true,
-    path = "/home/vcampello/.local/share/nvim/site/pack/packer/start/cmp-path",
+    path = "/Users/victorcampello/.local/share/nvim/site/pack/packer/start/cmp-path",
     url = "https://github.com/hrsh7th/cmp-path"
   },
   cmp_luasnip = {
     loaded = true,
-    path = "/home/vcampello/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
+    path = "/Users/victorcampello/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
     url = "https://github.com/saadparwaiz1/cmp_luasnip"
   },
   ["friendly-snippets"] = {
     loaded = true,
-    path = "/home/vcampello/.local/share/nvim/site/pack/packer/start/friendly-snippets",
+    path = "/Users/victorcampello/.local/share/nvim/site/pack/packer/start/friendly-snippets",
     url = "https://github.com/rafamadriz/friendly-snippets"
   },
   ["lsp-zero.nvim"] = {
     loaded = true,
-    path = "/home/vcampello/.local/share/nvim/site/pack/packer/start/lsp-zero.nvim",
+    path = "/Users/victorcampello/.local/share/nvim/site/pack/packer/start/lsp-zero.nvim",
     url = "https://github.com/VonHeikemen/lsp-zero.nvim"
   },
   ["mason-lspconfig.nvim"] = {
     loaded = true,
-    path = "/home/vcampello/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
+    path = "/Users/victorcampello/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
     url = "https://github.com/williamboman/mason-lspconfig.nvim"
   },
   ["mason.nvim"] = {
     loaded = true,
-    path = "/home/vcampello/.local/share/nvim/site/pack/packer/start/mason.nvim",
+    path = "/Users/victorcampello/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
   },
   ["nvim-cmp"] = {
     loaded = true,
-    path = "/home/vcampello/.local/share/nvim/site/pack/packer/start/nvim-cmp",
+    path = "/Users/victorcampello/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
-    path = "/home/vcampello/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    path = "/Users/victorcampello/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
   ["nvim-navic"] = {
     loaded = true,
-    path = "/home/vcampello/.local/share/nvim/site/pack/packer/start/nvim-navic",
+    path = "/Users/victorcampello/.local/share/nvim/site/pack/packer/start/nvim-navic",
     url = "https://github.com/SmiteshP/nvim-navic"
   },
   ["nvim-treesitter"] = {
     loaded = true,
-    path = "/home/vcampello/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    path = "/Users/victorcampello/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   onedarker = {
     config = { "\27LJ\2\n9\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\26colorscheme onedarker\bcmd\bvim\0" },
     loaded = true,
-    path = "/home/vcampello/.local/share/nvim/site/pack/packer/start/onedarker",
+    path = "/Users/victorcampello/.local/share/nvim/site/pack/packer/start/onedarker",
     url = "https://github.com/LunarVim/Onedarker.nvim"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "/home/vcampello/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    path = "/Users/victorcampello/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
   playground = {
     loaded = true,
-    path = "/home/vcampello/.local/share/nvim/site/pack/packer/start/playground",
+    path = "/Users/victorcampello/.local/share/nvim/site/pack/packer/start/playground",
     url = "https://github.com/nvim-treesitter/playground"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/home/vcampello/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    path = "/Users/victorcampello/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
   ["rose-pine"] = {
     config = { "\27LJ\2\n9\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\26colorscheme rose-pine\bcmd\bvim\0" },
     loaded = true,
-    path = "/home/vcampello/.local/share/nvim/site/pack/packer/start/rose-pine",
+    path = "/Users/victorcampello/.local/share/nvim/site/pack/packer/start/rose-pine",
     url = "https://github.com/rose-pine/neovim"
   },
   ["telescope.nvim"] = {
     loaded = true,
-    path = "/home/vcampello/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    path = "/Users/victorcampello/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
   undotree = {
     loaded = true,
-    path = "/home/vcampello/.local/share/nvim/site/pack/packer/start/undotree",
+    path = "/Users/victorcampello/.local/share/nvim/site/pack/packer/start/undotree",
     url = "https://github.com/mbbill/undotree"
   },
   ["vim-fugitive"] = {
     loaded = true,
-    path = "/home/vcampello/.local/share/nvim/site/pack/packer/start/vim-fugitive",
+    path = "/Users/victorcampello/.local/share/nvim/site/pack/packer/start/vim-fugitive",
     url = "https://github.com/tpope/vim-fugitive"
   },
   ["vim-signify"] = {
     loaded = true,
-    path = "/home/vcampello/.local/share/nvim/site/pack/packer/start/vim-signify",
+    path = "/Users/victorcampello/.local/share/nvim/site/pack/packer/start/vim-signify",
     url = "https://github.com/mhinz/vim-signify"
   }
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: onedarker
-time([[Config for onedarker]], true)
-try_loadstring("\27LJ\2\n9\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\26colorscheme onedarker\bcmd\bvim\0", "config", "onedarker")
-time([[Config for onedarker]], false)
 -- Config for: rose-pine
 time([[Config for rose-pine]], true)
 try_loadstring("\27LJ\2\n9\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\26colorscheme rose-pine\bcmd\bvim\0", "config", "rose-pine")
 time([[Config for rose-pine]], false)
+-- Config for: onedarker
+time([[Config for onedarker]], true)
+try_loadstring("\27LJ\2\n9\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\26colorscheme onedarker\bcmd\bvim\0", "config", "onedarker")
+time([[Config for onedarker]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
