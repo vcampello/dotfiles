3453767956e8cf2c1aca1398d7ad079d25ddefe7
Victor Campello <victor@ticker.co.uk>
2023-01-04 13:13:33 +0000

    Lots of changes

--- nvim/after/plugin/theme.lua
+++ nvim/after/plugin/theme.lua
@@ -1,9 +1,10 @@
 function ResetTheme()
-    color = color or "ayu-dark"
+    color = color or "carbonfox"
     vim.cmd.colorscheme(color)
 
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    vim.api.nvim_set_hl(0, "LineNr", { fg = "#FDDB27", bg = "#0f151e" })
+    vim.api.nvim_set_hl(0, "Normal", {})
+    vim.api.nvim_set_hl(0, "NormalFloat", {})
+    --vibrant yellow gutter
+    --vim.api.nvim_set_hl(0, "LineNr", { fg = "#FDDB27", bg = "#0f151e" })
 end
 ResetTheme()
--- nvim/lua/vcampello/packer.lua
+++ nvim/lua/vcampello/packer.lua
@@ -4,65 +4,63 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.0',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
 
-  -- Themes
-  use({
-	  'LunarVim/Onedarker.nvim',
-	  as = 'onedarker',
-	  config = function()
-		  vim.cmd('colorscheme onedarker')
-	  end
-  })
+    -- Themes
+    use('LunarVim/Onedarker.nvim')
+    use('Shatur/neovim-ayu')
+    use('EdenEast/nightfox.nvim')
 
-  use({
-      'Shatur/neovim-ayu',
-      as = 'ayu',
-      config = function()
-          vim.cmd('colorscheme ayu-dark')
-      end
-  })
+    -- Tools
+    use('nvim-treesitter/playground')
+    use('tpope/vim-fugitive')
+    use('mbbill/undotree')
+    use('mhinz/vim-signify')
+    use {
+        'nvim-tree/nvim-tree.lua',
+        as = 'nvim-tree',
+        tag = 'nightly' -- optional, updated every week. (see issue #1193)
+    }
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
+    use('nanozuki/tabby.nvim')
 
-  -- Syntax higlighting
-  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate'})
+    -- Syntax higlighting
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
 
-  -- Tools
-  use('nvim-treesitter/playground')
-  use('tpope/vim-fugitive')
-  use('mbbill/undotree')
-  use('mhinz/vim-signify')
+    -- LSP
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
 
-  -- LSP
-  use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  requires = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},
-		  {'williamboman/mason.nvim'},
-		  {'williamboman/mason-lspconfig.nvim'},
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-path' },
+            { 'saadparwaiz1/cmp_luasnip' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'hrsh7th/cmp-nvim-lua' },
 
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},
-		  {'hrsh7th/cmp-buffer'},
-		  {'hrsh7th/cmp-path'},
-		  {'saadparwaiz1/cmp_luasnip'},
-		  {'hrsh7th/cmp-nvim-lsp'},
-		  {'hrsh7th/cmp-nvim-lua'},
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },
+            { 'rafamadriz/friendly-snippets' },
+        }
+    }
 
-		  -- Snippets
-		  {'L3MON4D3/LuaSnip'},
-		  {'rafamadriz/friendly-snippets'},
-	  }
-  }
-
-  use {
-      "SmiteshP/nvim-navic",
-      requires = "neovim/nvim-lspconfig"
-  }
+    use {
+        "SmiteshP/nvim-navic",
+        requires = "neovim/nvim-lspconfig"
+    }
 end)
