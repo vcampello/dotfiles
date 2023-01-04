ec5593e7e52fe667cf2377e09f0f8e8fae1eba40
0BeyM3 <victor.campello.work@gmail.com>
2023-01-04 20:35:34 +0000

    Added windows support for undotree config

--- nvim/after/plugin/undotree.lua
+++ nvim/after/plugin/undotree.lua
@@ -1,1 +1,12 @@
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+
+-- Use a different path on Windows
+if vim.loop.os_uname().sysname == "Windows_NT"
+then
+    vim.opt.undodir = os.getenv("UserProfile") .. "/.nvim/undodir"
+
+else
+    vim.opt.undodir = os.getenv("HOME") .. "/.nvim/undodir"
+end
+
+vim.opt.undofile = true
--- nvim/lua/vcampello/set.lua
+++ nvim/lua/vcampello/set.lua
@@ -13,8 +13,6 @@ vim.opt.wrap = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.nvim/undodir"
-vim.opt.undofile = true
 
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
