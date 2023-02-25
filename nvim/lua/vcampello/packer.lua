dc78bd8c0387877c6d0b1061e1e25d69b43d4284
Victor Campello <victor.campello.work@gmail.com>
2023-02-25 15:16:14 +0000

    Removed old filetree plugin

--- nvim/lua/vcampello/packer.lua
+++ nvim/lua/vcampello/packer.lua
@@ -21,13 +21,6 @@ return require("packer").startup(function(use)
     use({ "tpope/vim-fugitive" })
     use({ "mbbill/undotree" })
     use({ "lewis6991/gitsigns.nvim" })
-    -- use({
-    --     "nvim-tree/nvim-tree.lua",
-    --     requires = {
-    --         "nvim-tree/nvim-web-devicons", -- optional, for file icons
-    --     },
-    --     tag = "nightly", -- optional, updated every week. (see issue #1193)
-    -- })
     use({
         "nvim-neo-tree/neo-tree.nvim",
         branch = "v2.x",
