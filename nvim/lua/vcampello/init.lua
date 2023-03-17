c10e8a9101b8c193fcbb775f9c98ac96473ef588
0BeyM3 <victor.campello.work@gmail.com>
2023-03-17 08:05:17 +0000

    Switched to lazy.nvim

--- nvim/lua/vcampello/init.lua
+++ nvim/lua/vcampello/init.lua
@@ -1,3 +1,4 @@
 require("vcampello.set") -- Must be required before remap because of the leader key
 require("vcampello.remap")
-require("vcampello.packer")
+-- require("vcampello.packer")
+require("vcampello.lazy")
---
+++ nvim/lua/vcampello/lazy.lua
@@ -0,0 +1,85 @@
+-- Make sure to set `mapleader` before lazy so your mappings are correct
+require("lazy").setup({
+
+    -- Themes
+    { "Shatur/neovim-ayu", lazy = false },
+    { "catppuccin/nvim", name = "catppuccin", lazy = false },
+    --
+    -- Tools
+    -- use('nvim-treesitter/playground')
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+    },
+    "stevearc/dressing.nvim",
+    "tpope/vim-fugitive",
+    "mbbill/undotree",
+    "lewis6991/gitsigns.nvim",
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+            "s1n7ax/nvim-window-picker",
+        },
+    },
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "kyazdani42/nvim-web-devicons" },
+    },
+    { "echasnovski/mini.nvim" },
+    { "numToStr/Comment.nvim" }, -- mini.comment doesn't support block comments
+    { "folke/which-key.nvim" },
+    { "NvChad/nvim-colorizer.lua" },
+    {
+        "folke/trouble.nvim",
+        dependencies = "kyazdani42/nvim-web-devicons",
+    },
+
+    {
+        "akinsho/toggleterm.nvim",
+    },
+    { "dstein64/vim-startuptime" },
+
+    -- Syntax higlighting
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate"  },
+    {
+        "windwp/nvim-ts-autotag",
+        dependencies = { "nvim-treesitter" },
+    },
+    -- LSP
+    {
+        "VonHeikemen/lsp-zero.nvim",
+        dependencies = {
+            -- LSP Support
+             "neovim/nvim-lspconfig" ,
+             "williamboman/mason.nvim" ,
+             "williamboman/mason-lspconfig.nvim" ,
+
+            -- Autocompletion
+             "hrsh7th/nvim-cmp" ,
+             "hrsh7th/cmp-buffer" ,
+             "hrsh7th/cmp-path" ,
+             "saadparwaiz1/cmp_luasnip" ,
+             "hrsh7th/cmp-nvim-lsp" ,
+             "hrsh7th/cmp-nvim-lua" ,
+
+            -- Snippets
+             "L3MON4D3/LuaSnip" ,
+             "rafamadriz/friendly-snippets" ,
+
+            -- Custom stuff
+             "b0o/schemastore.nvim" ,
+             "onsails/lspkind.nvim" ,
+             "jose-elias-alvarez/null-ls.nvim" ,
+             "JoosepAlviste/nvim-ts-context-commentstring" ,
+             "simrat39/rust-tools.nvim" ,
+        },
+    },
+
+    {
+        "SmiteshP/nvim-navic",
+        dependencies = { "neovim/nvim-lspconfig" },
+    },
+})
