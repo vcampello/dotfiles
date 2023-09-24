9fac98b6732ce8ad918dd7e3d3e711adad81bb82
Victor Campello <victor.campello.work@gmail.com>
2023-09-24 13:17:34 +0100

    changed nvim theme and configured wezterm

---
+++ wezterm.lua
@@ -0,0 +1,164 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = "carbonfox"
+config.font = wezterm.font("Victor Mono SemiBold")
+config.font_size = 10.5
+-- config.cell_width = 1.1
+
+config.window_background_opacity = 0.9
+config.tab_bar_at_bottom = true
+
+config.window_decorations = "RESIZE"
+-- Terminal style rendering
+config.use_fancy_tab_bar = false
+config.audible_bell = "Disabled"
+
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
+-- The filled in variant of the < symbol
+local SOLID_LEFT_ARROW = "<-" -- wezterm.nerdfonts.pl_right_hard_divider
+
+-- The filled in variant of the > symbol
+local SOLID_RIGHT_ARROW = wezterm.nerdfonts.pl_left_soft_divider
+
+-- This function returns the suggested title for a tab.
+-- It prefers the title that was set via `tab:set_title()`
+-- or `wezterm cli set-tab-title`, but falls back to the
+-- title of the active pane in that tab.
+function tab_title(tab_info)
+	local title = tab_info.tab_title
+	-- if the tab title is explicitly set, take that
+	if title and #title > 0 then
+		return title
+	end
+	-- Otherwise, use the title from the active pane
+	-- in that tab
+	return tab_info.active_pane.title
+end
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+	local background = "#ffffff"
+	local foreground = "#000000"
+
+	if tab.is_active then
+		background = "#ffffff"
+		foreground = "#000000"
+	elseif hover then
+		background = "#909090"
+		foreground = "#ffffff"
+	end
+
+	local title = tab_title(tab)
+
+	-- ensure that the titles fit in the available space,
+	-- and that we have room for the edges.
+	title = wezterm.truncate_right(title, max_width - 2)
+
+	return {
+		{ Background = { Color = background } },
+		{ Foreground = { Color = foreground } },
+		{ Text = "  " },
+		{ Text = title },
+		{ Text = " " .. SOLID_RIGHT_ARROW },
+	}
+end)
+
+config.inactive_pane_hsb = {
+	saturation = 0.8,
+	brightness = 0.5,
+}
+
+-- timeout_milliseconds defaults to 1000 and can be omitted
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
+config.keys = {
+	-- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
+	{
+		key = "a",
+		mods = "LEADER|CTRL",
+		action = wezterm.action.SendKey({ key = "a", mods = "CTRL" }),
+	},
+	{
+		key = "s",
+		mods = "LEADER",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "v",
+		mods = "LEADER",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "h",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "j",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+	{
+		key = "k",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "l",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		key = "c",
+		mods = "LEADER",
+		action = wezterm.action.CloseCurrentPane({ confirm = true }),
+	},
+	{
+		key = "z",
+		mods = "LEADER",
+		action = wezterm.action.TogglePaneZoomState,
+	},
+	{
+		key = "r",
+		mods = "LEADER",
+		action = wezterm.action.ActivateKeyTable({ name = "resize_pane", one_shot = false }),
+	},
+}
+
+config.key_tables = {
+	resize_pane = {
+		{ key = "h", action = wezterm.action.AdjustPaneSize({ "Left", 1 }) },
+		{ key = "j", action = wezterm.action.AdjustPaneSize({ "Down", 1 }) },
+		{ key = "k", action = wezterm.action.AdjustPaneSize({ "Up", 1 }) },
+		{ key = "l", action = wezterm.action.AdjustPaneSize({ "Right", 1 }) },
+		{
+			key = "r",
+			action = wezterm.action.RotatePanes("Clockwise"),
+		},
+		{ key = "Escape", action = "PopKeyTable" },
+		{ key = "Enter", action = "PopKeyTable" },
+	},
+}
+
+config.colors = {
+	-- The color of the split lines between panes
+	split = "#4e3773",
+}
+return config
