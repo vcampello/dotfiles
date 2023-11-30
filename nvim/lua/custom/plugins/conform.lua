328860e5de51a39f816d12ef4472fb041799e8d8
Victor Campello <victor@ticker.co.uk>
2023-11-30 14:09:02 +0000

    comform: fix ts formatting and allow autoformat to be disabled

--- nvim/lua/custom/plugins/conform.lua
+++ nvim/lua/custom/plugins/conform.lua
@@ -1,3 +1,24 @@
+-- enable/disable format on save
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+  if args.bang then
+    -- FormatDisable! will disable formatting just for this buffer
+    -- FIXME: why doens't the lsp like these?
+    vim.b.disable_autoformat = true
+  else
+    vim.g.disable_autoformat = true
+  end
+end, {
+  desc = "Disable autoformat-on-save",
+  bang = true,
+})
+
+vim.api.nvim_create_user_command("FormatEnable", function()
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
+end, {
+  desc = "Re-enable autoformat-on-save",
+})
+
 return {
   "stevearc/conform.nvim",
   opts = {
@@ -7,19 +28,24 @@ return {
       python = { "isort", "black" },
       -- Use a sub-list to run only the first available formatter
       -- TODO: turn this into a table so it's not so repetitive
-      javascript = { { "prettierd", "prettier" }, "eslint_d" },
+      javascript = { { "prettierd", "prettier" } },
+      typescript = { { "prettierd", "prettier" } },
       json = { "prettierd", "prettier" },
       markdown = { "prettierd", "prettier" },
       rust = { "rustfmt" },
       shell = { "shellcheck" },
+      ["*"] = { "codespell" },
     },
-    format_on_save = {
-      lsp_fallback = true,
-      timeout_ms = 500,
-    },
+    format_on_save = function(bufnr)
+      -- Disable with a global or buffer-local variable
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        return
+      end
+      return { timeout_ms = 500, lsp_fallback = true }
+    end,
+    init = function()
+      -- If you want the formatexpr, here is the place to set it
+      vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    end,
   },
-  init = function()
-    -- If you want the formatexpr, here is the place to set it
-    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-  end,
 }
