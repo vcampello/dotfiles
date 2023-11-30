d5dc46f89b48fc0431dd87a75d15043f4318f14e
vcampello <victor.campello.work@gmail.com>
2023-11-30 21:47:46 +0000

    chore: move gitsigns to dedicated file

--- nvim/init.lua
+++ nvim/init.lua
@@ -70,49 +70,6 @@ require("lazy").setup({
   -- Useful plugin to show you pending keybinds.
   { "folke/which-key.nvim", opts = {} },
   {
-    -- Adds git related signs to the gutter, as well as utilities for managing changes
-    "lewis6991/gitsigns.nvim",
-    opts = {
-      -- See `:help gitsigns.txt`
-      signs = {
-        add = { text = "+" },
-        change = { text = "~" },
-        delete = { text = "_" },
-        topdelete = { text = "‾" },
-        changedelete = { text = "~" },
-      },
-      on_attach = function(bufnr)
-        vim.keymap.set(
-          "n",
-          "<leader>gp",
-          require("gitsigns").preview_hunk,
-          { buffer = bufnr, desc = "Git Hunk Preview " }
-        )
-
-        -- don't override the built-in and fugitive keymaps
-        local gs = package.loaded.gitsigns
-        vim.keymap.set({ "n", "v" }, "]g", function()
-          if vim.wo.diff then
-            return "]g"
-          end
-          vim.schedule(function()
-            gs.next_hunk()
-          end)
-          return "<Ignore>"
-        end, { expr = true, buffer = bufnr, desc = "Jump to next hunk" })
-        vim.keymap.set({ "n", "v" }, "[g", function()
-          if vim.wo.diff then
-            return "[g"
-          end
-          vim.schedule(function()
-            gs.prev_hunk()
-          end)
-          return "<Ignore>"
-        end, { expr = true, buffer = bufnr, desc = "Jump to previous hunk" })
-      end,
-    },
-  },
-  {
     "EdenEast/nightfox.nvim",
     priority = 1000,
     config = function()
