3506d60f41bacb827bcae29cdc022fd1da4464c7
vcampello <victor.campello.work@gmail.com>
2023-12-03 22:06:17 +0000

    telescope: improvements

--- nvim/init.lua
+++ nvim/init.lua
@@ -242,6 +242,42 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 -- Enable telescope fzf native, if installed
 pcall(require("telescope").load_extension, "fzf")
 
+-- Telescope live_grep in git root
+-- Function to find the git root directory based on the current buffer's path
+local function find_git_root()
+  -- Use the current buffer's path as the starting point for the git search
+  local current_file = vim.api.nvim_buf_get_name(0)
+  local current_dir
+  local cwd = vim.fn.getcwd()
+  -- If the buffer is not associated with a file, return nil
+  if current_file == "" then
+    current_dir = cwd
+  else
+    -- Extract the directory from the current file's path
+    current_dir = vim.fn.fnamemodify(current_file, ":h")
+  end
+
+  -- Find the Git root directory from the current file's path
+  local git_root = vim.fn.systemlist("git -C " .. vim.fn.escape(current_dir, " ") .. " rev-parse --show-toplevel")[1]
+  if vim.v.shell_error ~= 0 then
+    print("Not a git repository. Searching on current working directory")
+    return cwd
+  end
+  return git_root
+end
+
+-- Custom live_grep function to search in git root
+local function live_grep_git_root()
+  local git_root = find_git_root()
+  if git_root then
+    require("telescope.builtin").live_grep({
+      search_dirs = { git_root },
+    })
+  end
+end
+
+vim.api.nvim_create_user_command("LiveGrepGitRoot", live_grep_git_root, {})
+
 -- See `:help telescope.builtin`
 vim.keymap.set("n", "<leader>?", require("telescope.builtin").oldfiles, { desc = "Find recently opened files" })
 vim.keymap.set("n", "<leader><space>", require("telescope.builtin").buffers, { desc = "Find existing buffers" })
@@ -252,7 +288,7 @@ vim.keymap.set(
   { desc = "Fuzzily search in current buffer" }
 )
 
-vim.keymap.set("n", "<leader>sa", function()
+vim.keymap.set("n", "<leader>fa", function()
   -- Show hidden files
   -- https://github.com/skbolton/titan/blob/4d0d31cc6439a7565523b1018bec54e3e8bc502c/nvim/nvim/lua/mappings/filesystem.lua#L6
   require("telescope.builtin").find_files({ find_command = { "rg", "--files", "--hidden", "-g", "!.git" } })
@@ -262,6 +298,7 @@ vim.keymap.set("n", "<leader>ff", require("telescope.builtin").git_files, { desc
 vim.keymap.set("n", "<leader>fd", require("telescope.builtin").diagnostics, { desc = "Search Diagnostics" })
 vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files, { desc = "Search Files" })
 vim.keymap.set("n", "<leader>fg", require("telescope.builtin").live_grep, { desc = "Search by Grep" })
+vim.keymap.set("n", "<leader>fG", ":LiveGrepGitRoot<cr>", { desc = "Search by Grep on Git Root" })
 vim.keymap.set("n", "<leader>fh", require("telescope.builtin").help_tags, { desc = "Search Help" })
 vim.keymap.set("n", "<leader>fm", require("telescope.builtin").marks, { desc = "Search Marks" })
 vim.keymap.set("n", "<leader>fk", require("telescope.builtin").keymaps, { desc = "Search Keymaps" })
