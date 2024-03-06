36518308574948af5ae70a312a8355f15548fff9
Victor Campello <victor@ticker.co.uk>
2024-03-06 09:01:11 +0000

    dressing: enable vim motions on popups

--- nvim/lua/vcampello/plugins/dressing.lua
+++ nvim/lua/vcampello/plugins/dressing.lua
@@ -1,3 +1,13 @@
 return {
   "stevearc/dressing.nvim",
+  config = function()
+    require("dressing").setup({
+      input = {
+        -- enable vim motions on floating inputs like LSP rename
+        insert_only = false,
+        -- start in normal mode
+        start_in_insert = false,
+      },
+    })
+  end,
 }
