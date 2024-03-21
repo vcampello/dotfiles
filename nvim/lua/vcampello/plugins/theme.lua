894b4e171d324395c15e71bf54f086078bf3adfd
Victor Campello <victor.campello.work@gmail.com>
2024-03-21 22:00:22 +0000

    nvim: make comments darker

--- nvim/lua/vcampello/plugins/theme.lua
+++ nvim/lua/vcampello/plugins/theme.lua
@@ -7,6 +7,10 @@ return {
       -- NOTE: to use the light theme, set `vim.o.background = 'light'`
       style = "vulgaris", -- Choose between 'vulgaris' (regular), 'multiplex' (greener), and 'light'
       transparent = true,
+      highlights = {
+        -- make comments blend nicely with background, similar to other color schemes
+        ["@comment"] = { fg = "$grey" },
+      },
     })
     require("bamboo").load()
   end,
