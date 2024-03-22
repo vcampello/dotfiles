f1bfcf80d013651f332ac931baf19eda0959aa72
Victor Campello <victor@ticker.co.uk>
2024-03-22 09:37:48 +0000

    neotree: enable normal mode for popups

--- nvim/lua/vcampello/plugins/neotree.lua
+++ nvim/lua/vcampello/plugins/neotree.lua
@@ -80,6 +80,25 @@ return {
           enabled = true,
         },
       },
+      event_handlers = {
+
+        {
+          event = "neo_tree_popup_input_ready",
+          handler = function()
+            -- enter input popup with normal mode by default.
+            vim.cmd("stopinsert")
+          end,
+        },
+        {
+          event = "neo_tree_popup_input_ready",
+          ---@param args { bufnr: integer, winid: integer }
+          handler = function(args)
+            -- map <esc> to enter normal mode (by default closes prompt)
+            -- don't forget `opts.buffer` to specify the buffer of the popup.
+            vim.keymap.set("i", "<esc>", vim.cmd.stopinsert, { noremap = true, buffer = args.bufnr })
+          end,
+        },
+      },
     })
 
     -- Mappings
