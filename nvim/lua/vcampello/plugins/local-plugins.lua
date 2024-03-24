391645c7063ff29187d7bc0307231647a5272c0e
Victor Campello <victor.campello.work@gmail.com>
2024-03-24 20:37:12 +0000

    nvim: remove test file

--- nvim/lua/vcampello/plugins/local-plugins.lua
+++
@@ -1,9 +0,0 @@
-local plugin_path = vim.fn.stdpath("config") .. "/lua/vcampello/local-plugins/neotree-sessions.nvim"
-print(plugin_path)
-return {
-  lazy = false,
-  dir = plugin_path,
-  opts = {
-    msg = "potato",
-  },
-}
