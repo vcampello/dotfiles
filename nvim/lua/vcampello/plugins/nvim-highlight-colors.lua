b979968c90504710d5cc78a4b3904fdee7287268
Victor Campello <victor@ticker.co.uk>
2024-06-03 09:25:23 +0100

    feat: swap nvim-colorizer for nvim-highlight-colors

---
+++ nvim/lua/vcampello/plugins/nvim-highlight-colors.lua
@@ -0,0 +1,9 @@
+return {
+  "brenoprata10/nvim-highlight-colors",
+  opts = {
+    render = "virtual",
+    virtual_symbol_position = "eow",
+    virtual_symbol_prefix = " ",
+    virtual_symbol_suffix = "",
+  },
+}
