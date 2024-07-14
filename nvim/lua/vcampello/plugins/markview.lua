571d84e17a4bc85a8e89fa070864f3269dd65135
vcampello <victor.campello.work@gmail.com>
2024-07-14 22:07:32 +0100

    feat: added markbiew

---
+++ nvim/lua/vcampello/plugins/markview.lua
@@ -0,0 +1,11 @@
+return {
+  enabled = true,
+  "OXY2DEV/markview.nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons", -- Used by the code bloxks
+  },
+
+  config = function()
+    require("markview").setup()
+  end,
+}
