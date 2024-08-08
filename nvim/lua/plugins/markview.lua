5bfd8efebcefca19e6fe33445b0d030005473500
Victor Campello <victor@ticker.co.uk>
2024-08-08 07:23:54 +0100

    markview: re-enable in hybrid mode

--- nvim/lua/plugins/markview.lua
+++ nvim/lua/plugins/markview.lua
@@ -1,11 +1,25 @@
 return {
-  enabled = false,
+  enabled = true,
   "OXY2DEV/markview.nvim",
   dependencies = {
     "nvim-tree/nvim-web-devicons", -- Used by the code bloxks
   },
 
   config = function()
-    require("markview").setup()
+    require("markview").setup({
+      modes = { "n", "no", "c" }, -- Change these modes
+      -- to what you need
+
+      hybrid_modes = { "n" }, -- Uses this feature on
+      -- normal mode
+
+      -- This is nice to have
+      callbacks = {
+        on_enable = function(_, win)
+          vim.wo[win].conceallevel = 2
+          vim.wo[win].concealcursor = "c"
+        end,
+      },
+    })
   end,
 }
