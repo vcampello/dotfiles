7d2fb72c75207132fd1052aa05c76b17274c94f5
vcampello <victor.campello.work@gmail.com>
2024-08-12 18:40:42 +0100

    feat: split and rewrite to-do list

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -42,7 +42,7 @@ function M.settings()
   -- Set completeopt to have a better completion experience
   vim.o.completeopt = "menuone,noselect"
 
-  -- NOTE: You should make sure your terminal supports this
+  -- NOTE: make sure your terminal supports this
   vim.o.termguicolors = true
   vim.o.guicursor = "n-v-c-i-sm:block-blinkwait7000-blinkon400-blinkoff250,i-ci-ve:ver25,r-cr-o:hor20"
   vim.o.cursorline = true
@@ -102,129 +102,46 @@ end
 
 function M.default_remaps()
   -- [[ Basic Keymaps ]]
+  local map = vim.keymap.set
+
+  -- Disable space action so it can act as the leader
+  map({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
   -- Keep cursor centered when moving or searching
-  vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down (centralised)" })
-  vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up (centralised)" })
-  vim.keymap.set("n", "n", "nzzzv", { desc = "Next search match (centralised)" })
-  vim.keymap.set("n", "N", "Nzzzv", { desc = "Previous search match (centralised)" })
+  map("n", "<C-d>", "<C-d>zz", { desc = "Scroll down (centralised)" })
+  map("n", "<C-u>", "<C-u>zz", { desc = "Scroll up (centralised)" })
+  map("n", "n", "nzzzv", { desc = "Next search match (centralised)" })
+  map("n", "N", "Nzzzv", { desc = "Previous search match (centralised)" })
 
-  vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "Delete into the void and paste without losing buffer" })
+  map("x", "<leader>p", [["_dP]], { desc = "Delete into the void and paste without losing buffer" })
 
   -- Window navigation (will get overridden by smart-splits)
-  vim.keymap.set("n", "<C-H>", "<C-w>h", { desc = "Focus on left window" })
-  vim.keymap.set("n", "<C-J>", "<C-w>j", { desc = "Focus on below window" })
-  vim.keymap.set("n", "<C-K>", "<C-w>k", { desc = "Focus on above window" })
-  vim.keymap.set("n", "<C-L>", "<C-w>l", { desc = "Focus on right window" })
-
-  -- Keymaps for better default experience
-  -- See `:help vim.keymap.set()`
-  vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
+  map("n", "<C-H>", "<C-w>h", { desc = "Focus on left window" })
+  map("n", "<C-J>", "<C-w>j", { desc = "Focus on below window" })
+  map("n", "<C-K>", "<C-w>k", { desc = "Focus on above window" })
+  map("n", "<C-L>", "<C-w>l", { desc = "Focus on right window" })
 
   -- Remap for dealing with word wrap
-  vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-  vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+  map("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+  map("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
-  -- Better terminal navigation, also used for escaping
-  vim.keymap.set("t", "<C-H>", "<C-\\><C-N><C-H>", { desc = "Escape term and focus on left window" })
-  vim.keymap.set("t", "<C-J>", "<C-\\><C-N><C-J>", { desc = "Escape term and focus on below window" })
-  vim.keymap.set("t", "<C-K>", "<C-\\><C-N><C-K>", { desc = "Escape term and focus on above window" })
-  vim.keymap.set("t", "<C-L>", "<C-\\><C-N><C-L>", { desc = "Escape term and focus on right window" })
-
-  -- TODO: setup group
+  -- Better terminal navigation and start terminal in insert mode
   vim.api.nvim_create_autocmd({ "TermOpen" }, {
+    group = vim.api.nvim_create_augroup("InitTerminal", { clear = true }),
     callback = function()
-      -- Start terminal in insert mod
+      map("t", "<C-H>", "<C-\\><C-N><C-H>", { desc = "Escape term and focus on left window" })
+      map("t", "<C-J>", "<C-\\><C-N><C-J>", { desc = "Escape term and focus on below window" })
+      map("t", "<C-K>", "<C-\\><C-N><C-K>", { desc = "Escape term and focus on above window" })
+      map("t", "<C-L>", "<C-\\><C-N><C-L>", { desc = "Escape term and focus on right window" })
       vim.cmd.startinsert()
     end,
   })
-end
-
---TODO: move this somewhere else as it's technically a plugin
---Open a to-do list
-function M.setup_todo_list()
-  -- support for a float per tab
-  local todo_map = {}
-
-  local function todo_list()
-    local todo_filepath = vim.fn.stdpath("data") .. "/my-todo.md"
-    local buf = vim.fn.bufadd(todo_filepath)
-    local tab_id = vim.api.nvim_get_current_tabpage()
-    local win_id = vim.api.nvim_get_current_win()
-
-    if todo_map[tab_id] ~= nil then
-      print("A todo list is already open on this tab. Windows: " .. vim.inspect(todo_map, { newline = " " }))
-      return
-    end
-
-    -- current window dimensions
-    local win_width = vim.api.nvim_list_uis()[1].width
-    local win_height = vim.api.nvim_list_uis()[1].height
-
-    -- floating window dimensions
-    local width = math.floor(win_width * 0.5)
-    local height = math.floor(win_height * 0.7)
-
-    -- anchor offset to centre it
-    local col = math.floor((win_width / 2) - (width / 2))
-    local row = math.floor((win_height / 2) - (height / 2))
-
-    -- open floating window
-    local todo_win_id = vim.api.nvim_open_win(buf, true, {
-      relative = "editor",
-      row = row,
-      col = col,
-      width = width,
-      height = height,
-      title = " To-do ",
-      title_pos = "center",
-      border = "rounded",
-    })
-
-    -- TODO: setup group
-    vim.api.nvim_create_autocmd({ "WinClosed" }, {
-      buffer = buf,
-      once = false, -- the buffer is shared so each window needs the ability to run the callback
-      callback = function()
-        -- only remove from hashmap if closing the floating window from the same tab
-        if tab_id == vim.api.nvim_get_current_tabpage() then
-          -- print("Removing window from todo_map " .. os.date("%c"))
-          todo_map[tab_id] = nil
-        end
-      end,
-    })
-
-    -- TODO: clean this up
-    vim.api.nvim_create_autocmd({ "BufWinLeave" }, {
-      buffer = buf,
-      once = false,
-      callback = function()
-        -- only remove from hashmap if closing the floating window from the same tab
-        if tab_id == vim.api.nvim_get_current_tabpage() then
-          -- print("Removing window from todo_map " .. os.date("%c"))
-          todo_map[tab_id] = nil
-        end
-
-        -- also close the window if the buffer is changed - e.g. prevent jumping to another file
-        vim.api.nvim_win_close(vim.api.nvim_get_current_win(), true)
-      end,
-    })
-
-    -- enter the window
-    vim.api.nvim_set_current_win(todo_win_id)
-    -- store window id to prevent opening it again
-    todo_map[tab_id] = win_id
-    -- print(vim.inspect(todo_map))
-  end
-
-  vim.keymap.set("n", "<leader>t", todo_list, { desc = "Open to-do list", silent = true })
 end
 
 function M.setup()
   M.settings()
   M.define_signs()
   M.default_remaps()
-  M.setup_todo_list()
 end
 
 M.setup()
---
+++ nvim/lua/core/todo-list.lua
@@ -0,0 +1,166 @@
+local utils = require("lib.utils")
+
+local M = {
+  name = "Simple to-do list",
+  desc = "A simple to-do list accessible in any vim instance and with with multitab support",
+  config = {
+    --- Filepath for todo list markdown file
+    filepath = vim.fn.stdpath("data") .. "/my-todo.md",
+    keymap = {
+      quick_close = "q",
+    },
+  },
+  actions = {},
+}
+
+---Track if it's already open somwehere
+---@type integer | nil
+local known_winid = nil
+
+---Autocommand group for todo-list
+local todo_augroup = vim.api.nvim_create_augroup("TodolistGroup", { clear = true })
+
+---Open to-do list window
+---@return integer winid
+local function create_and_open()
+  local bufnr = vim.fn.bufadd(M.config.filepath)
+  -- ensure buffer is wiped out when it's hidden (prevents jumpting to it when using c-o)
+  vim.bo[bufnr].bufhidden = "wipe"
+  -- prevent buffer from being listed by :ls
+  vim.bo[bufnr].buflisted = true
+
+  -- current window dimensions
+  local win_width = vim.api.nvim_list_uis()[1].width
+  local win_height = vim.api.nvim_list_uis()[1].height
+
+  -- floating window dimensions
+  local width = math.floor(win_width * 0.5)
+  local height = math.floor(win_height * 0.7)
+
+  -- anchor offset to centre it
+  local col = math.floor((win_width / 2) - (width / 2))
+  local row = math.floor((win_height / 2) - (height / 2))
+
+  -- open floating window
+  local winid = vim.api.nvim_open_win(bufnr, true, {
+    relative = "editor",
+    row = row,
+    col = col,
+    width = width,
+    height = height,
+    title = " To-do ",
+    title_pos = "center",
+    border = "rounded",
+  })
+
+  vim.api.nvim_create_autocmd({ "BufWinLeave", "BufLeave", "WinClosed" }, {
+    desc = "Automatically close the floating window and prevent it being used for other files",
+    group = todo_augroup,
+    buffer = bufnr,
+    once = false,
+    callback = function()
+      M.actions.close()
+    end,
+  })
+
+  vim.api.nvim_create_autocmd({ "ModeChanged" }, {
+    desc = "automatically save buffer when going back to normal mode",
+    group = todo_augroup,
+    buffer = bufnr,
+    once = false,
+    callback = function(ev)
+      if string.match(ev.match, "%a:n") then
+        vim.cmd.write()
+      end
+    end,
+  })
+
+  -- Quick escape keymap
+  vim.keymap.set({ "n" }, M.config.keymap.quick_close, function()
+    M.actions.close()
+  end, {
+    buffer = bufnr,
+    desc = "Quick close to-do list",
+    nowait = true,
+    silent = true,
+  })
+
+  -- focus
+  vim.api.nvim_set_current_win(winid)
+
+  return winid
+end
+
+---Close floating window
+M.actions.close = function()
+  if known_winid ~= nil and vim.api.nvim_win_is_valid(known_winid) then
+    vim.api.nvim_win_close(known_winid, true)
+  end
+  known_winid = nil
+end
+
+---Check if is open on tab
+---@return boolean
+M.is_open_on_tab = function(tabid)
+  for _, value in ipairs(vim.api.nvim_tabpage_list_wins(tabid)) do
+    if value == known_winid then
+      return true
+    end
+  end
+
+  return false
+end
+
+---Open window
+M.actions.open = function()
+  local tabid = vim.api.nvim_get_current_tabpage()
+
+  if not M.is_open_on_tab(tabid) then
+    known_winid = create_and_open()
+  end
+end
+
+---Toggle floating window
+M.actions.toggle = function()
+  local tabid = vim.api.nvim_get_current_tabpage()
+
+  if M.is_open_on_tab(tabid) then
+    M.actions.close()
+  else
+    -- attemp to close
+    M.actions.close()
+
+    -- open and store window id to prevent opening it again
+    known_winid = create_and_open()
+  end
+end
+
+---Dump current known location
+M.actions.debug = function()
+  if known_winid ~= nil then
+    print("Open on window: " .. known_winid)
+  else
+    print("Not open")
+  end
+end
+
+---Initialise plugin
+M.setup = function()
+  -- Setup commands
+  vim.api.nvim_create_user_command("TodoList", function(opts)
+    local action_name = opts.fargs[1]
+    if M.actions[action_name] then
+      M.actions[action_name](opts)
+    end
+  end, {
+    nargs = 1,
+    complete = function()
+      return utils.tableKeys(M.actions)
+    end,
+  })
+
+  -- default keymap
+  vim.keymap.set("n", "<leader>t", M.actions.toggle, { desc = "Toggle to-do list", silent = true })
+end
+
+return M
