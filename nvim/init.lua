447b8efc35a45426aaf1e0f744fa2837ea62707a
vcampello <victor.campello.work@gmail.com>
2024-08-13 21:49:35 +0100

    fix: go_to diagnostic deprecation

--- nvim/init.lua
+++ nvim/init.lua
@@ -155,8 +155,12 @@ vim.keymap.set("n", "<leader>fm", fzflua.marks, { desc = "Search marks" })
 vim.keymap.set("n", "<leader>ls", fzflua.spell_suggest, { desc = "Search spell suggestions" })
 
 -- Diagnostic keymaps
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
+vim.keymap.set("n", "[d", function()
+  vim.diagnostic.jump({ count = -1, float = true })
+end, { desc = "Go to previous diagnostic message" })
+vim.keymap.set("n", "]d", function()
+  vim.diagnostic.jump({ count = 1, float = true })
+end, { desc = "Go to next diagnostic message" })
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
 
