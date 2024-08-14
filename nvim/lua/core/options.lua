e89dd70367c6185482e5a74b6692e88357944f57
vcampello <victor.campello.work@gmail.com>
2024-08-14 17:53:49 +0100

    feat: remove custom signs
    It's nicer without them

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -93,13 +93,6 @@ function M.settings()
   })
 end
 
-function M.define_signs()
-  vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
-  vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
-  vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
-  vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
-end
-
 function M.default_remaps()
   -- [[ Basic Keymaps ]]
   local map = vim.keymap.set
@@ -140,7 +133,6 @@ end
 
 function M.setup()
   M.settings()
-  M.define_signs()
   M.default_remaps()
 end
 
