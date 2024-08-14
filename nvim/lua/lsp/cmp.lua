0f98488e3ae95daf881db43d4577dbc44c43b81d
vcampello <victor.campello.work@gmail.com>
2024-08-14 22:45:41 +0100

    chore: move cmp to its own spec

---
+++ nvim/lua/lsp/cmp.lua
@@ -0,0 +1,121 @@
+return {
+  -- Autocompletion
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    -- eye candy
+    "onsails/lspkind.nvim",
+
+    -- Snippet Engine & its associated nvim-cmp source
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
+
+    "hrsh7th/cmp-nvim-lsp", -- adds LSP completion capabilities
+    "FelipeLema/cmp-async-path", -- adds filesystem paths.
+    "rafamadriz/friendly-snippets", -- adds a number of user-friendly snippets
+    {
+      "f3fora/cmp-spell", -- adds spellcheck
+      config = function()
+        vim.opt.spell = true
+        vim.opt.spelllang = { "en_gb" }
+      end,
+    },
+    {
+      "David-Kunz/cmp-npm",
+      dependencies = { "nvim-lua/plenary.nvim" },
+      ft = "json",
+      config = function()
+        require("cmp-npm").setup({})
+      end,
+    },
+  },
+  config = function()
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+    require("luasnip.loaders.from_vscode").lazy_load()
+    luasnip.config.setup({})
+
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      completion = {
+        completeopt = "menu,menuone,noinsert",
+      },
+      mapping = cmp.mapping.preset.insert({
+
+        ["<C-n>"] = cmp.mapping.select_next_item(),
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-u>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+        ["<C-e>"] = cmp.mapping.abort(),
+        -- ["<CR>"] = cmp.mapping.confirm({
+        --   behavior = cmp.ConfirmBehavior.Replace,
+        --   select = true,
+        -- }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_locally_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.locally_jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      }),
+      sources = {
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "async_path" },
+        { name = "npm", keyword_length = 4 },
+        {
+          name = "spell",
+          option = {
+            keep_all_entries = false,
+            enable_in_context = function()
+              return require("cmp.config.context").in_treesitter_capture("spell")
+            end,
+          },
+        },
+      },
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
+      formatting = {
+        fields = { "kind", "abbr", "menu" },
+        expandable_indicator = true,
+        format = function(entry, vim_item)
+          local kind = require("lspkind").cmp_format({
+            mode = "symbol_text",
+            maxwidth = 100,
+          })(entry, vim_item)
+          local strings = vim.split(kind.kind, "%s", { trimempty = true })
+          kind.kind = " " .. (strings[1] or "") .. " "
+          kind.menu = "    (" .. (strings[2] or "") .. ")"
+
+          return kind
+        end,
+      },
+    })
+    vim.diagnostic.config({
+      float = { border = "single" },
+    })
+
+    require("lspconfig.ui.windows").default_options = {
+      border = "single",
+    }
+  end,
+}
