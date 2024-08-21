74d6fe2960be7a49f42276803f4185165c53c8a1
vcampello <victor.campello.work@gmail.com>
2024-08-21 21:02:38 +0100

    chore: remove custom prod title

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -158,26 +158,15 @@ wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_wid
   local title = tab_title(tab)
   ---@cast title  string
 
-  local title_contains_prod = title:lower():match("prod")
-
-  local prod_colour = "#990000"
-  --danger danger
-  if title_contains_prod then
-    title = "DANGER: " .. title
-  end
-
   local elements = {}
 
   if tab.is_active then
     table.insert(elements, { Foreground = { Color = "#ffffff" } })
-    table.insert(elements, { Background = { Color = title_contains_prod and prod_colour or "#104060" } })
-  elseif title_contains_prod then
-    table.insert(elements, { Foreground = { Color = prod_colour } })
-    table.insert(elements, { Background = { Color = "#ffffff" } })
+    table.insert(elements, { Background = { Color = "#104060" } })
   end
 
   if hover then
-    table.insert(elements, { Foreground = { Color = title_contains_prod and prod_colour or "#000000" } })
+    table.insert(elements, { Foreground = { Color = "#000000" } })
     table.insert(elements, { Background = { Color = "#ffffff" } })
   end
 
