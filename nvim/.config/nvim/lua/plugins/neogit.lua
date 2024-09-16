fca28d35df6e1ad39857df8a4043cb25166dd490
Victor Campello <victor@ticker.co.uk>
2024-09-16 07:01:17 +0100

    fix: setup neogit

--- nvim/.config/nvim/lua/plugins/neogit.lua
+++ nvim/.config/nvim/lua/plugins/neogit.lua
@@ -7,6 +7,7 @@ return {
   },
   config = function()
     local neogit = require("neogit")
+    neogit.setup({})
     vim.keymap.set({ "n" }, "<leader>gg", neogit.open, { desc = "Open Neogit", nowait = true, silent = true })
   end,
 }
