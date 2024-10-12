5a7bc6358f9e051abdaf11108cc912e6ead4cc2f
Victor Campello <victor.campello.work@gmail.com>
2024-10-12 09:45:35 +0100

    nvim: update markview

--- nvim/.config/nvim/lua/plugins/markview.lua
+++ nvim/.config/nvim/lua/plugins/markview.lua
@@ -1,26 +1,11 @@
 return {
   enabled = true,
-  lazy = false,
   "OXY2DEV/markview.nvim",
+  lazy = false, -- Recommended
+  -- ft = "markdown" -- If you decide to lazy-load anyway
+
   dependencies = {
-    "nvim-tree/nvim-web-devicons", -- Used by the code bloxks
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons",
   },
-
-  config = function()
-    require("markview").setup({
-      modes = { "n", "no", "c" }, -- Change these modes
-      -- to what you need
-
-      hybrid_modes = { "n" }, -- Uses this feature on
-      -- normal mode
-
-      -- This is nice to have
-      callbacks = {
-        on_enable = function(_, win)
-          vim.wo[win].conceallevel = 2
-          vim.wo[win].concealcursor = "c"
-        end,
-      },
-    })
-  end,
 }
