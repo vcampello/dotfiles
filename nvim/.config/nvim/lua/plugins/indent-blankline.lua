ed6c8d7e374274c6a50f29c475fea0e55466fe9a
Victor Campello <victor.campello.work@gmail.com>
2024-10-16 22:05:50 +0100

    nvim: dynamic indent blankline highlights

--- nvim/.config/nvim/lua/plugins/indent-blankline.lua
+++ nvim/.config/nvim/lua/plugins/indent-blankline.lua
@@ -18,21 +18,18 @@ return {
       "RainbowViolet",
       "RainbowCyan",
     }
+    -- Define colorscheme from RainbowDelimiters
+    local highlight = {
+      "RainbowDelimiterRed",
+      "RainbowDelimiterYellow",
+      "RainbowDelimiterBlue",
+      "RainbowDelimiterOrange",
+      "RainbowDelimiterGreen",
+      "RainbowDelimiterViolet",
+      "RainbowDelimiterCyan",
+    }
     local hooks = require("ibl.hooks")
-    -- create the highlight groups in the highlight setup hook, so they are reset
-    -- every time the colorscheme changes
-    hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-      local hl = vim.api.nvim_set_hl
-      hl(0, "RainbowRed", { fg = "#E06C75" })
-      hl(0, "RainbowYellow", { fg = "#E5C07B" })
-      hl(0, "RainbowBlue", { fg = "#61AFEF" })
-      hl(0, "RainbowOrange", { fg = "#D19A66" })
-      hl(0, "RainbowGreen", { fg = "#98C379" })
-      hl(0, "RainbowViolet", { fg = "#C678DD" })
-      hl(0, "RainbowCyan", { fg = "#56B6C2" })
-    end)
 
-    vim.g.rainbow_delimiters = { highlight = highlight }
     require("ibl").setup({ scope = { highlight = highlight } })
 
     hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
