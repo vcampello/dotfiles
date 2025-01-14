41d40c9ead655ca18bb934050f122c28f9054c40
Victor Campello <victor@ticker.co.uk>
2025-01-14 11:36:10 +0000

    nvim: format lsp log

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -101,3 +101,6 @@ local workspace_path = vim.fn.getcwd()
 local cache_dir = vim.fn.stdpath("data")
 local unique_id = vim.fn.fnamemodify(workspace_path, ":t") .. "_" .. vim.fn.sha256(workspace_path):sub(1, 8) ---@type string
 vim.opt.shadafile = cache_dir .. "/myshada/" .. unique_id .. ".shada"
+
+vim.lsp.set_log_level("trace")
+require("vim.lsp.log").set_format_func(vim.inspect)
