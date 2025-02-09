57d98b986e996cc7dc4bd54bd72dd0e282f005e9
Victor Campello <victor.campello.work@gmail.com>
2025-02-09 22:39:58 +0000

    fix: neotree mapping conflict with diagnostics

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -84,7 +84,7 @@ return {
 
     -- Mappings
     -- vim.cmd([[nnoremap \ :Neotree reveal<cr>]])
-    vim.keymap.set("n", "<leader>e", "<cmd>:Neotree toggle float<cr>", { desc = "Neotree (float)" })
-    vim.keymap.set("n", "<leader>er", "<cmd>:Neotree toggle right<cr>", { desc = "Neotree (right)" })
+    vim.keymap.set("n", "<leader>o", "<cmd>:Neotree toggle float<cr>", { desc = "Neotree (float)" })
+    vim.keymap.set("n", "<leader>O", "<cmd>:Neotree toggle right<cr>", { desc = "Neotree (right)" })
   end,
 }
