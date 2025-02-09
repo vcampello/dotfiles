2955baf87f64cdf4a6373af0d8082225c6e0b651
Victor Campello <victor.campello.work@gmail.com>
2025-02-09 21:22:56 +0000

    feat: change undotree mapping from u to U

--- nvim/.config/nvim/lua/plugins/undotree.lua
+++ nvim/.config/nvim/lua/plugins/undotree.lua
@@ -1,7 +1,7 @@
 return {
   "mbbill/undotree",
   config = function()
-    vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Toggle UndoTree" })
+    vim.keymap.set("n", "<leader>U", vim.cmd.UndotreeToggle, { desc = "Toggle UndoTree" })
     vim.opt.undodir = vim.fn.stdpath("state") .. "/undodir"
     vim.opt.undofile = true
   end,
