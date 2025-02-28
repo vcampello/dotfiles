e6fd2e32f5460994c65602db973a877f91734578
Victor Campello <victor@ticker.co.uk>
2025-02-28 09:29:09 +0000

    feat: autoformat large json files with jq

--- nvim/.config/nvim/lua/core/autocmds.lua
+++ nvim/.config/nvim/lua/core/autocmds.lua
@@ -34,3 +34,27 @@ vim.api.nvim_create_autocmd("FileType", {
     })
   end,
 })
+
+vim.api.nvim_create_autocmd("BufRead", {
+  group = core_group,
+  pattern = "*.json",
+  callback = function(ev)
+    -- vim.print(ev)
+    local max_filesize_in_bytes = 100 * 1024 -- 100 KB
+    local ok, stats = pcall(vim.uv.fs_stat, ev.match) -- ev.match would also work in this case
+
+    if ok and stats and stats.size > max_filesize_in_bytes then
+      -- stop it here if jq is missing before retrieving the line content
+      if vim.fn.executable("jq") == 0 then
+        vim.notify("jq not found to format large JSON file", vim.log.levels.WARN)
+        return
+      end
+
+      if #vim.api.nvim_get_current_line() > 300 then
+        vim.cmd("%!jq .") -- this may conflict with formatters
+        vim.notify("Large JSON file formatted", vim.log.levels.INFO)
+        -- vim.cmd.write() -- if it should be safed
+      end
+    end
+  end,
+})
--- nvim/.config/nvim/lua/lsp/treesitter.lua
+++ nvim/.config/nvim/lua/lsp/treesitter.lua
@@ -34,16 +34,17 @@ return {
       ignore_install = {},
       auto_install = true,
       sync_install = false,
-      -- disable slow treesitter highlight for large files
-      disable = function(lang, buf)
-        local max_filesize = 100 * 1024 -- 100 KB
-        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-        if ok and stats and stats.size > max_filesize then
-          print("Large file detected. Disabling treesitter")
-          return true
-        end
-      end,
-      highlight = { enable = true },
+      highlight = {
+        enable = true,
+        -- disable slow treesitter highlight for large files
+        disable = function(lang, buf)
+          local max_filesize = 100 * 1024 -- 100 KB
+          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
+            return true
+          end
+        end,
+      },
       indent = { enable = false },
       incremental_selection = {
         enable = true,
