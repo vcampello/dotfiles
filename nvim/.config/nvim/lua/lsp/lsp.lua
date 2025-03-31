2315e5dfb8a19d6605facd92f75504f76a3fab2d
Victor Campello <victor@ticker.co.uk>
2025-03-31 10:08:20 +0100

    chore: preserve native gr commands

--- nvim/.config/nvim/lua/lsp/lsp.lua
+++ nvim/.config/nvim/lua/lsp/lsp.lua
@@ -1,18 +1,6 @@
--- Resolve differences between neovim nightly (version 0.11) and stable (version 0.10)
----@param client vim.lsp.Client
----@param method vim.lsp.protocol.Method
----@param bufnr? integer some lsp support methods only in specific files
----@return boolean
-local function client_supports_method(client, method, bufnr)
-  if vim.fn.has("nvim-0.11") == 1 then
-    return client:supports_method(method, bufnr)
-  else
-    return client.supports_method(method, { bufnr = bufnr })
-  end
-end
-
 return {
   -- lazy = false,
+  event = "LspAttach",
   -- LSP Configuration & Plugins
   "neovim/nvim-lspconfig",
   dependencies = {
@@ -31,104 +19,71 @@ return {
     "antosha417/nvim-lsp-file-operations",
   },
   config = function()
-    vim.api.nvim_create_autocmd("LspAttach", {
-      group = vim.api.nvim_create_augroup("InitLspOnAttach", { clear = true }),
-      callback = function(event)
-        local bufnr = event.buf
-        local client = vim.lsp.get_client_by_id(event.data.client_id)
-
-        ---Keymap wrapper
-        ---@param mode string | string[]
-        ---@param keys string
-        ---@param func fun()
-        ---@param desc string
-        local function map(mode, keys, func, desc)
-          vim.keymap.set(mode, keys, func, { buffer = bufnr, desc = "LSP: " .. desc })
+    --  This function gets run when an LSP connects to a particular buffer.
+    local on_attach = function(client, bufnr)
+      local nmap = function(keys, func, desc)
+        if desc then
+          desc = "LSP: " .. desc
         end
 
-        -- setup inlay hints if available
-        if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
-          map("n", "grI", function()
-            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr }))
-          end, "Toggle inlay hints")
+        vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+      end
+      local imap = function(keys, func, desc)
+        if desc then
+          desc = "LSP: " .. desc
         end
 
-        local fzf = require("fzf-lua")
-        -- override: vim.lsp.buf.code_action
-        map("n", "gra", fzf.lsp_code_actions, "Code Action")
+        vim.keymap.set("i", keys, func, { buffer = bufnr, desc = desc })
+      end
 
-        --- these 2 are used way too often
-        map("n", "gd", function()
-          fzf.lsp_definitions({ jump1 = true })
-        end, "Go to Definition")
-        map("n", "gD", vim.lsp.buf.type_definition, "Go to Type Definition")
+      -- NOTE: this makes text shift horizontally and it can be distracting
+      local inlay_hint_filter = { bufnr = bufnr }
+      if client.server_capabilities.inlayHintProvider then
+        vim.lsp.inlay_hint.enable(false, inlay_hint_filter)
+      end
 
-        -- override: vim.lsp.buf.references
-        map("n", "grr", function()
-          fzf.lsp_references({ jump1 = true })
-        end, "Go to References")
+      nmap("<leader>li", function()
+        local is_enabled = vim.lsp.inlay_hint.is_enabled(inlay_hint_filter)
+        vim.lsp.inlay_hint.enable(not is_enabled, inlay_hint_filter)
+      end, "Toggle inlay hints")
 
-        -- override: vim.lsp.buf_impementation
-        map("n", "gri", function()
-          fzf.lsp_implementations({ jump1 = true })
-        end, "Goto Implementation")
+      local fzf = require("fzf-lua")
+      nmap("<leader>lr", vim.lsp.buf.rename, "Rename")
+      -- nmap("<leader>la", vim.lsp.buf.code_action, "Code Action")
+      nmap("<leader>la", fzf.lsp_code_actions, "Code Action")
 
-        -- override: vim.lsp.buf.document_symbols
-        map("n", "gO", fzf.lsp_document_symbols, "Document Symbols")
+      nmap("gd", function()
+        fzf.lsp_definitions({ jump1 = true })
+      end, "Definitions")
+      nmap("grr", function()
+        fzf.lsp_references({ jump1 = true })
+      end, "References")
+      nmap("gri", function()
+        fzf.lsp_implementations({ jump1 = true })
+      end, "Implementation")
+      nmap("<leader>fs", fzf.lsp_document_symbols, "Document Symbols")
 
-        -- See `:help K` for why this keymap
-        map("n", "K", vim.lsp.buf.hover, "Hover Documentation")
-        map("n", "<leader>k", vim.lsp.buf.signature_help, "Signature Documentation")
-        --handled by blink
-        -- map("i", "<c-k>", vim.lsp.buf.signature_help, "Signature Documentation")
+      -- See `:help K` for why this keymap
+      nmap("K", vim.lsp.buf.hover, "Hover Documentation")
+      nmap("<leader>k", vim.lsp.buf.signature_help, "Signature Documentation")
+      imap("<c-k>", vim.lsp.buf.signature_help, "Signature Documentation")
 
-        -- Lesser used LSP functionality
-        map("n", "grA", vim.lsp.buf.add_workspace_folder, "Workspace Add Folder")
-        map("n", "grR", vim.lsp.buf.remove_workspace_folder, "Workspace Remove Folder")
-        map("n", "grL", function()
-          vim.print(vim.lsp.buf.list_workspace_folders())
-        end, "Workspace List Folders")
+      -- Lesser used LSP functionality
+      nmap("gD", vim.lsp.buf.type_definition, "Goto Type Definition")
+      nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "Workspace Add Folder")
+      nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "Workspace Remove Folder")
+      nmap("<leader>wl", function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+      end, "Workspace List Folders")
 
-        -- TODO: improve this
-        local function format()
-          require("conform").format({ async = true, lsp_fallback = true })
-        end
+      local function format()
+        require("conform").format({ async = true, lsp_fallback = true })
+      end
 
-        vim.api.nvim_buf_create_user_command(bufnr, "Format", format, { desc = "Format current buffer with LSP" })
+      vim.api.nvim_buf_create_user_command(bufnr, "Format", format, { desc = "Format current buffer with LSP" })
 
-        map("n", "grf", format, "Format current buffer with LSP")
-
-        -- The following two autocommands are used to highlight references of the
-        -- word under your cursor when your cursor rests there for a little while.
-        --    See `:help CursorHold` for information about when this is executed
-        --
-        -- When you move your cursor, the highlights will be cleared (the second autocommand).
-        if
-          client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf)
-        then
-          local highlight_augroup = vim.api.nvim_create_augroup("lsp-highlight", { clear = false })
-          vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-            buffer = event.buf,
-            group = highlight_augroup,
-            callback = vim.lsp.buf.document_highlight,
-          })
-
-          vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-            buffer = event.buf,
-            group = highlight_augroup,
-            callback = vim.lsp.buf.clear_references,
-          })
-
-          vim.api.nvim_create_autocmd("LspDetach", {
-            group = vim.api.nvim_create_augroup("lsp-detach", { clear = true }),
-            callback = function(ev)
-              vim.lsp.buf.clear_references()
-              vim.api.nvim_clear_autocmds({ group = highlight_augroup, buffer = ev.buf })
-            end,
-          })
-        end
-      end,
-    })
+      nmap("<leader>lf", format, "Format current buffer with LSP")
+    end
 
     -- Enable the following language servers
     local servers = {
@@ -136,6 +91,7 @@ return {
       terraformls = {},
       tflint = {},
 
+      -- other things
       clangd = {},
       pyright = {},
       rust_analyzer = {},
@@ -153,11 +109,8 @@ return {
         },
       },
       sqlls = {},
-
-      -- js/ts stuff
-      -- NOTE: replaced by typescript-tools
-      -- ts_ls = {},
-      volar = {},
+      -- NOTE: replaced by typescript-tools on_attach
+      ts_ls = {},
       eslint = {},
       graphql = {},
       html = {
@@ -172,7 +125,6 @@ return {
         settings = {
           Lua = {
             hint = { enable = true, paramType = true },
-            completion = { callSnippet = "Replace" },
             format = {
               enable = false,
             },
@@ -181,8 +133,6 @@ return {
             diagnostics = {
               -- Get the language server to recognize the `vim` global
               globals = { "vim", "Snacks" },
-              -- Ignore noisy warnings
-              disable = { "missing-fields" },
             },
           },
         },
@@ -213,75 +163,68 @@ return {
       nil_ls = {}, -- Nix support
     }
 
+    -- broadcast addional capabilities to servers
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    local cmp_capabilities = require("blink.cmp").get_lsp_capabilities(capabilities)
+    local file_ops_capabilities = require("lsp-file-operations").default_capabilities()
+    capabilities = vim.tbl_deep_extend("force", capabilities, cmp_capabilities, file_ops_capabilities)
+
+    require("mason").setup({ ui = { border = "single" } })
+    -- Ensure the servers above are installed
+    local mason_lspconfig = require("mason-lspconfig")
+
     -- nix is required for nil_ls to be installed
     if vim.fn.executable("nix") == 0 then
       servers.nil_ls = nil
     end
 
-    -- broadcast addional capabilities to servers
-    local default_capabilities = vim.lsp.protocol.make_client_capabilities()
-    local cmp_capabilities = require("blink.cmp").get_lsp_capabilities(default_capabilities)
-    local file_ops_capabilities = require("lsp-file-operations").default_capabilities()
-    local capabilities = vim.tbl_deep_extend("force", default_capabilities, cmp_capabilities, file_ops_capabilities)
-
-    ---@diagnostic disable-next-line: missing-fields
-    require("mason").setup({ ui = { border = "single" } })
-
-    local modifiedHandlers = {
-      ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-        border = "rounded",
-        -- Keep it open while typing
-        close_events = { "CursorMoved", "BufHidden" },
-      }),
-    }
-
-    -- setup normal lsp configs
-    require("mason-lspconfig").setup({
+    mason_lspconfig.setup({
       ensure_installed = vim.tbl_keys(servers),
-      automatic_installation = false,
-      handlers = {
-        function(server_name)
-          local server = servers[server_name] or {}
-          --- overrides
-          server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-          server.handlers = vim.tbl_deep_extend("force", {}, modifiedHandlers, server.handlers or {})
-          require("lspconfig")[server_name].setup(server)
-        end,
-      },
+      automatic_installation = true,
     })
 
-    -- setup ts_ls wrapper
-    require("typescript-tools").setup({
-      handlers = modifiedHandlers,
-      -- on_attach = on_attach,
-      filetypes = {
-        "javascript",
-        "javascriptreact",
-        "typescript",
-        "typescriptreact",
-        "vue",
-      },
-      settings = {
-        expose_as_code_action = "all",
-        tsserver_file_preferences = {
-          includeInlayParameterNameHints = "all",
-          includeInlayEnumMemberValueHints = true,
-          includeInlayFunctionLikeReturnTypeHints = true,
-          includeInlayFunctionParameterTypeHints = true,
-          includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-          includeInlayPropertyDeclarationTypeHints = true,
-          -- includeInlayVariableTypeHints = true,
-        },
-        capabilities = capabilities,
-        -- TODO: setup deno support
-        -- root_dir = root_pattern_excludes({
-        --   root = { "package.json" },
-        --   exclude = { "deno.json", "deno.jsonc" },
-        -- }),
-        tsserver_plugins = {
-          "@vue/typescript-plugin",
-        },
-      },
+    mason_lspconfig.setup_handlers({
+      function(server_name)
+        local handlers = {
+          ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
+          ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+            border = "rounded",
+            -- Keep it open while typing
+            close_events = { "CursorMoved", "BufHidden" },
+          }),
+        }
+
+        if server_name == "ts_ls" then
+          -- print("Setting up typescript-tools instead of ts_ls")
+          -- It was done this way so if it's removed I won't forget why ts_ls isn't working
+          require("typescript-tools").setup({
+            handlers = handlers,
+            on_attach = on_attach,
+            settings = {
+              expose_as_code_action = "all",
+              -- TODO: migrate this to ts_ls (the structure is different)
+              tsserver_file_preferences = {
+                includeInlayParameterNameHints = "all",
+                includeInlayEnumMemberValueHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                -- includeInlayVariableTypeHints = true,
+              },
+            },
+          })
+          return
+        end
+
+        require("lspconfig")[server_name].setup({
+          capabilities = capabilities,
+          on_attach = on_attach,
+          settings = (servers[server_name] or {}).settings,
+          filetypes = (servers[server_name] or {}).filetypes,
+          handlers = handlers,
+        })
+      end,
     })
   end,
 }
