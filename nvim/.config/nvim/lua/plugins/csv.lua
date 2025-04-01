d9cf6dd07c7768ae528263ec3dad2608fddfd470
Victor Campello <victor@ticker.co.uk>
2025-04-01 15:38:33 +0100

    feat: add csvview

---
+++ nvim/.config/nvim/lua/plugins/csv.lua
@@ -0,0 +1,20 @@
+return {
+  ft = "csv",
+  "hat0uma/csvview.nvim",
+  config = function()
+    require("csvview").setup({
+      view = {
+        spacing = 4,
+        display_mode = "border",
+      },
+    })
+
+    vim.api.nvim_create_autocmd("BufRead", {
+      group = vim.api.nvim_create_augroup("AutoEnableCsvView", { clear = true }),
+      pattern = "*.csv",
+      callback = function(ev)
+        vim.cmd.CsvViewEnable()
+      end,
+    })
+  end,
+}
