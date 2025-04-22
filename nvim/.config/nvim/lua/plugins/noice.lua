c4d6aef160ae4fc83d5038e9b7b7b3286c910a37
Victor Campello <victor@ticker.co.uk>
2025-04-22 15:07:04 +0100

    feat: add noice

---
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,41 @@
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  config = function()
+    local noice = require("noice").setup({
+
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+        },
+      },
+      -- you can enable a preset for easier configuration
+      presets = {
+        bottom_search = false, -- use a classic bottom cmdline for search
+        command_palette = false, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = true, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = true, -- add a border to hover docs and signature help
+      },
+    })
+
+    -- use the same leader pattern as other fzf keymaps
+    vim.keymap.set("n", "<leader>fn", ":Noice fzf<cr>", { desc = "Search Noice", nowait = true })
+
+    -- TEMP FIX until winborder is supported by Nui https://github.com/MunifTanjim/nui.nvim/pull/403
+    -- local initialWinborder = vim.o.winborder
+    -- vim.api.nvim_create_autocmd("CmdlineEnter", {
+    --   callback = function()
+    --     vim.o.winborder = "none"
+    --   end,
+    -- })
+    -- vim.api.nvim_create_autocmd("CmdlineLeave", {
+    --   callback = function()
+    --     vim.o.winborder = initialWinborder
+    --   end,
+    -- })
+  end,
+}
