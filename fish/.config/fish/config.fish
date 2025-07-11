05725a4ddf8809196737d1bf2f457f21886e707f
Victor Campello <victor.campello.work@gmail.com>
2025-07-11 23:37:28 +0100

    feat: add grc

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,14 +7,14 @@ fish_add_path $HOME/bin
 if test -f /home/linuxbrew/.linuxbrew/bin/brew
     # linux
     /home/linuxbrew/.linuxbrew/bin/brew shellenv | source
-else if test -f /opt/homebrew/bin/brew 
+else if test -f /opt/homebrew/bin/brew
     # macos
     /opt/homebrew/bin/brew shellenv | source
 end
 
 # activate mise by default
 mise activate fish | source
-mise completion fish > ~/.config/fish/completions/mise.fish # TODO: cache if this ever becomes a bottleneck
+mise completion fish >~/.config/fish/completions/mise.fish # TODO: cache if this ever becomes a bottleneck
 
 # Commands to run in interactive sessions can go here
 if status is-interactive
@@ -33,4 +33,7 @@ if status is-interactive
     set -gx EDITOR nvim # default editor
     set -gx MANPAGER 'nvim +Man!' # default manpage pager
     set -gx MANWIDTH 999 # manpage width
+
+    # grc
+    source $HOMEBREW_PREFIX/etc/grc.fish
 end
