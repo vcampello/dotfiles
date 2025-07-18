2f611603d02a250b009ca42a02995c5be7063add
Victor Campello <victor.campello.work@gmail.com>
2025-07-18 23:35:01 +0100

    feat: migrate circleci lsp to after/lsp

---
+++ nvim/.config/nvim/after/lsp/circleci_lsp.lua
@@ -0,0 +1,23 @@
+local r = require("lspconfig.util")
+vim.lsp.config("circleci_lsp", {
+  init_options = { hostInfo = "neovim" },
+  cmd = {
+    "circleci-yaml-language-server",
+    "-stdio",
+    "-schema",
+    vim.fn.stdpath("data") .. "/mason/packages/circleci-yaml-language-server/schema.json",
+  },
+  filetypes = {
+    "yaml",
+  },
+  root_markers = { ".git", ".circleci" },
+  root_dir = function(bufnr, on_dir)
+    print(vim.fn.bufname(bufnr))
+    -- only start when the file is in the .circleci folder
+    local bufname = vim.fn.bufname(bufnr)
+    local is_circle_ci_config = bufname:match("%.circleci/")
+    if is_circle_ci_config then
+      on_dir(vim.fn.getcwd())
+    end
+  end,
+})
