324d7774d615c801625e9b063cc3b2cb5c4d99a6
Victor Campello <victor@ticker.co.uk>
2025-08-11 16:39:41 +0100

    feat: improve treesitter auto install

--- nvim/.config/nvim/lua/lsp/treesitter.lua
+++ nvim/.config/nvim/lua/lsp/treesitter.lua
@@ -1,3 +1,39 @@
+---@class ConfigEntry
+---@field enable_otter boolean Enable other embedded lsp
+---@field ignore boolean Do not run autocommand
+---@field maps_to string Convert filetype to another parser name - e.g. typescriptreact -> tsx
+---@field installed boolean Prevent reprocessing and infinite calls when sending notifications inside of the autocmd
+
+local M = {}
+---@type ConfigEntry[]
+M.configs = {}
+
+---@param ft string
+---@param entry? ConfigEntry
+---@return ConfigEntry
+function M:update(ft, entry)
+  local opts = entry or {}
+
+  ---@type ConfigEntry
+  local defaults = {
+    enable_otter = false,
+    ignore = false,
+    -- install either the override or the lang itself
+    maps_to = opts.maps_to or ft,
+    installed = false,
+  }
+
+  local new_config = vim.tbl_extend("force", defaults, M.configs[ft] or {}, opts)
+  self.configs[ft] = new_config
+
+  return new_config
+end
+
+-- overrides
+M:update("javascriptreact", { maps_to = "jsx" })
+M:update("typescriptreact", { maps_to = "tsx" })
+M:update("toml", { enable_otter = true })
+
 return {
   "nvim-treesitter/nvim-treesitter",
   lazy = false,
@@ -7,29 +43,39 @@ return {
     "jmbuhr/otter.nvim",
   },
   config = function()
-    ---@type table<string, boolean>
-    local enable_otter = {
-      toml = true,
-    }
-
     vim.api.nvim_create_autocmd("FileType", {
       pattern = "*",
       callback = function(ev)
-        local lang = ev.match
+        local ft = ev.match
+        local bufnr = ev.buf
+        local config = M:update(ft)
 
-        -- we cannot auto install it
-        if require("nvim-treesitter.parsers")[lang] == nil then
+        if config.ignore then
           return
         end
 
-        -- schedule the auto install to prevent locking up neovim
-        return vim.schedule(function()
-          require("nvim-treesitter").install(lang):wait()
-          vim.treesitter.start(ev.buf, lang)
+        -- it cannot be auto installed
+        if not config.installed and require("nvim-treesitter.parsers")[config.maps_to] == nil then
+          M:update(ft, { ignore = true, installed = true })
+          return
+        end
 
-          if enable_otter[lang] then
+        -- all good
+        if config.installed then
+          vim.treesitter.start(bufnr, config.maps_to)
+          if config.enable_otter then
             require("otter").activate()
           end
+        end
+
+        -- try to install it without blocking and prevent reprocessing
+        -- otter will only be enabled in the next run
+        vim.schedule(function()
+          require("nvim-treesitter").install(config.maps_to):wait()
+          M:update(ft, { ignore = false, installed = true })
+
+          -- enable on original buffer
+          vim.treesitter.start(bufnr, config.maps_to)
         end)
       end,
     })
