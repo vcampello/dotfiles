7395ff2be42fdf481397a61fec5f36ce963e7fd5
Victor Campello <victor.campello.work@gmail.com>
2025-10-25 21:05:01 +0100

    feat: remove some non-default keymaps

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -30,16 +30,4 @@ vim.api.nvim_create_autocmd({ "TermOpen" }, {
   end,
 })
 
--- Diagnostic keymaps
-vim.keymap.set("n", "[d", function()
-  vim.diagnostic.jump({ count = -1, float = true, wrap = true })
-  vim.cmd.normal("zz") -- centralise after jumping
-end, { desc = "Go to previous diagnostic message" })
-vim.keymap.set("n", "]d", function()
-  vim.diagnostic.jump({ count = 1, float = true, wrap = true })
-  vim.cmd.normal("zz") -- centralise after jumping
-end, { desc = "Go to next diagnostic message" })
-
-vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
 vim.keymap.set("x", "g/", "<Esc>/\\%V", { desc = "Search visual selection" })
