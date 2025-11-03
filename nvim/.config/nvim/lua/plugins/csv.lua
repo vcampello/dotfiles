940834bcc19512e2c60506261bca63038a87ea4a
vcampello <victor@ticker.co.uk>
2025-11-03 15:41:54 +0000

    feat: add csv related keymaps

--- nvim/.config/nvim/lua/plugins/csv.lua
+++ nvim/.config/nvim/lua/plugins/csv.lua
@@ -7,12 +7,25 @@ return {
         spacing = 4,
         display_mode = "border",
       },
+      keymaps = {
+        -- Horizontal navigation
+        jump_next_field_end = { "<Tab>", mode = { "n", "v" } },
+        jump_prev_field_end = { "<S-Tab>", mode = { "n", "v" } },
+
+        -- Vertical navigation
+        jump_next_row = { "<Enter>", mode = { "n", "v" } },
+        jump_prev_row = { "<S-Enter>", mode = { "n", "v" } },
+
+        -- Select field content
+        textobject_field_inner = { "if", mode = { "o", "x" } },
+        textobject_field_outer = { "af", mode = { "o", "x" } },
+      },
     })
 
     vim.api.nvim_create_autocmd("BufRead", {
       group = vim.api.nvim_create_augroup("AutoEnableCsvView", { clear = true }),
       pattern = "*.csv",
-      callback = function(ev)
+      callback = function()
         vim.cmd.CsvViewEnable()
         vim.o.wrap = false
       end,
