bbb2b76bd0bbc3afc8595834482547c96c6963b3
Victor Campello <victor@ticker.co.uk>
2025-12-10 10:38:26 +0000

    feat: remove undotree as it's built-in

--- nvim/.config/nvim/lua/plugins/undotree.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "mbbill/undotree",
-  config = function()
-    vim.keymap.set("n", "<leader>U", vim.cmd.UndotreeToggle, { desc = "Toggle UndoTree" })
-    vim.opt.undodir = vim.fn.stdpath("state") .. "/undodir"
-    vim.opt.undofile = true
-  end,
-}
