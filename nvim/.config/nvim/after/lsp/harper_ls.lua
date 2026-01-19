bb818801a97deaec3abf6ddf2e15082d3a077143
Victor Campello <victor@ticker.co.uk>
2026-01-19 09:10:39 +0000

    fix: change harper to en_gb

--- nvim/.config/nvim/after/lsp/harper_ls.lua
+++ nvim/.config/nvim/after/lsp/harper_ls.lua
@@ -1,9 +1,9 @@
 return {
   settings = {
     ["harper-ls"] = {
+      dialect = "British",
       linters = {
         SentenceCapitalization = false,
-        SpellCheck = false,
       },
     },
   },
