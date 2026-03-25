1b66adfdbd7f56ecc86f504cc67bf75bb2efc818
Victor Campello <victor.campello.work@gmail.com>
2026-03-25 22:53:24 +0000

    feat: update nvim colorscheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -65,4 +65,32 @@ function M.nordic()
   }
 end
 
-return M.nordic()
+function M.everviolet()
+  return {
+    "everviolet/nvim",
+    name = "evergarden",
+    priority = 1000, -- Colorscheme plugin is loaded first before any other plugins
+    config = function()
+      require("evergarden").setup({
+        theme = {
+          variant = "fall", -- 'winter'|'fall'|'spring'|'summer'
+          accent = "green",
+        },
+        editor = {
+          transparent_background = true,
+          sign = { color = "none" },
+          float = {
+            color = "mantle",
+            solid_border = false,
+          },
+          completion = {
+            color = "surface0",
+          },
+        },
+      })
+      vim.cmd.colorscheme("evergarden")
+    end,
+  }
+end
+
+return M.everviolet()
