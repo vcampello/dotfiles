25de9843737f1ab415b2868d71cb3c10da6c3d07
Victor Campello <victor.campello.work@gmail.com>
2026-06-14 21:37:58 +0100

    feat: rework autopairs
    I wanted to switch to mini.pairs but it doesn't seem to support html/xml

--- nvim/.config/nvim/lua/lsp/autopairs.lua
+++ nvim/.config/nvim/lua/lsp/autopairs.lua
@@ -1,19 +1,7 @@
 return {
     "windwp/nvim-autopairs",
     dependencies = {
-        {
-            "windwp/nvim-ts-autotag",
-            config = function()
-                require("nvim-ts-autotag").setup({
-                    opts = {
-                        -- Defaults
-                        enable_close = true, -- Auto close tags
-                        enable_rename = true, -- Auto rename pairs of tags
-                        enable_close_on_slash = true, -- Auto close on trailing </
-                    },
-                })
-            end,
-        },
+        { "windwp/nvim-ts-autotag", opts = {} },
     },
     opts = {},
 }
