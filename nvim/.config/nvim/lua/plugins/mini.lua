402ac9a53d236a6acf11fab7a8a80bde030b9d1e
Victor Campello <victor.campello.work@gmail.com>
2026-06-14 20:36:21 +0100

    feat: replace several plugins with mini.*
    replace the following with mini variants:
    
    - nvim-highlight-colors
    - snacks (anything that was enabled)
    - todo-comments

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -1,5 +1,23 @@
 return {
-    "nvim-mini/mini.splitjoin",
+    "https://github.com/nvim-mini/mini.nvim",
     version = false,
-    opts = {},
+    config = function()
+        require("mini.indentscope").setup()
+        require("mini.splitjoin").setup()
+        require("mini.ai").setup()
+
+        local hipatterns = require("mini.hipatterns")
+        hipatterns.setup({
+            highlighters = {
+                -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
+                fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
+                hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
+                todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+                note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+
+                -- Highlight hex color strings (`#rrggbb`) using that color
+                hex_color = hipatterns.gen_highlighter.hex_color(),
+            },
+        })
+    end,
 }
