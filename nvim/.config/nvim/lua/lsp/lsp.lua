8789ec74b41ea3bff766534be442b8d5c9721684
Victor Campello <victor.campello.work@gmail.com>
2026-06-16 22:00:27 +0100

    feat: remove redundant :LspRestart

--- nvim/.config/nvim/lua/lsp/lsp.lua
+++ nvim/.config/nvim/lua/lsp/lsp.lua
@@ -1,18 +1,3 @@
-local function restartLsps()
-    local clients = vim.lsp.get_clients({ bufnr = 0 })
-
-    for _, v in ipairs(clients) do
-        v:stop()
-    end
-    vim.cmd.update()
-    vim.defer_fn(vim.cmd.edit, 1000)
-end
-
-vim.api.nvim_create_user_command("LspRestart", restartLsps, {
-    desc = "Restart all LSPs attached to the currect buffer",
-    nargs = 0,
-})
-
 return {
     -- lazy = false,
     -- LSP Configuration & Plugins
